ff75e8b71d3c96dc830ebf284ae9140c3e277180
kusakabenanami <138275920+kusakabenanami@users.noreply.github.com>
2023-07-19 13:23:39 +0000

    [Add]4-27

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  def show
+    @user =　User.find(params[:id])
+    @post_images = @user.post_images
+  end
+
+  def edit
+  end
+
+  def view
+  end
+end
